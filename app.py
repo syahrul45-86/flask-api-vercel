2133d12a2a54f5fd64c9d78dfe262a719149f8a5
syahrul45-86 <rullsyahrul4586@gmail.com>
2025-11-15 03:56:10 +0700

    update config for vercel new

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from config import app, db
 from Routes.User_bp import user_bp
 from Routes.Level_bp import level_bp
 
-# register blueprint
+# === ROUTE ROOT UNTUK TEST ===
+@app.route("/")
+def home():
+    return {"message": "API is running on Vercel"}
+
+# === REGISTER BLUEPRINT ===
 app.register_blueprint(user_bp)
 app.register_blueprint(level_bp)
 
-# Saat di local (run manual)
+# === LOCAL DEVELOPMENT MODE ===
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)  # berjalan hanya local
+    app.run(debug=True)
 
-
-
-# Vercel akan mencari variabel bernama 'app'
+# === HANDLER UNTUK VERCEL ===
 handler = app
